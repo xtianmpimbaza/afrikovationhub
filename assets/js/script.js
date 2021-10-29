088d46636f359c68b1234509ba94e30a396ea799
Eng Christian <xtianm4@gmail.com>
2021-10-29 14:48:52 +0300

    project done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
         e.preventDefault();
         const PBFKey = "FLWPUBK_TEST-3bb6cd9b2f4c842811e1a052c84fb9dc-X"; // paste in the public key from your dashboard here
         const txRef = ''+Math.floor((Math.random() * 1000000000) + 1); //Generate a random id for the transaction reference
-        // const email = document.getElementById('email').value;
-        // const phone = document.getElementById('phone').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const name = document.getElementById('name').value;
         const amount = document.getElementById('amount').value;
 
 
         // getpaidSetup is Rave's inline script function. it holds the payment data to pass to Rave.
         getpaidSetup({
             PBFPubKey: PBFKey,
-            customer_email: 'xtianm4@gmail.com',
             amount: amount,
-            // customer_phone: phone,
             currency: "USD",  // Select the currency. leaving it empty defaults to NGN
             txref: txRef, // Pass your UNIQUE TRANSACTION REFERENCE HERE.
             onclose: function() {
                 console.log('Closed payment')
+            },
+            customer: {
+                email: email,
+                phone_number: phone,
+                name: name,
             },
             callback: function(response) {
                 flw_ref = response.tx.flwRef;// collect flwRef returned and pass to a server page to complete status check.
